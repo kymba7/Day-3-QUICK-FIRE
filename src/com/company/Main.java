33fca4d2d71e8fad78bc7a5f80a6a5e766eb97d9
Kymbrlee <kymba7@gmail.com>
2018-01-10 10:59:38 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello! Hola! Bonjour! What is your name?");
+        Scanner friend = new Scanner(System.in);
+        String inputfromuser = friend.nextLine();
+        System.out.println("My name is "+inputfromuser);
+        //if (inputfromuser.equales (inputfromuser))[System.out.println("Hello +inputfromuser");]
+    }
+}
